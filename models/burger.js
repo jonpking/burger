98c544e7aca80ef42142ff4210324ab3a8e20f6f
Enigma <jonpking@gmail.com>
2019-08-21 18:21:57 -0700

    updated code for burger models

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,27 @@
 const orm = require("../config/orm.js");
 
+const burger = {
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res)
+        });
+    },
+    create: function (cols, vals, cb) {
+        orm.select("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+    delete: function (condition, cb) {
+        orm.delete("burgers", condition, function (res) {
+            cb(res);
+        });
+    }
+};
 orm.selectAll("burgers");
 
 orm.insertOne("burgers", "burger_name", "hawaiian burger");
