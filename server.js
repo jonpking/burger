31b000612a319cdb2777be481eb4b0ca173b31a1
Enigma <jonpking@gmail.com>
2019-08-21 22:47:09 -0700

    set up server code

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+require("./controllers/burgers_controller.js")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
