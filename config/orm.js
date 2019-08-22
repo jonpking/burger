6221faa95dff5dcce4539bb91f524dd1ca244bc4
Enigma <jonpking@gmail.com>
2019-08-21 18:07:29 -0700

    updated code for insertOne and updateOne

--- config/orm.js
+++ config/orm.js
@@ -8,25 +8,25 @@ const orm = {
             console.log(result);
         });
     },
-    insertOne: function (tableInput, colAndValArray) {
-        let columns = colAndValArray.keys().join(", ");
-        let values = colAndValArray.values().join(", ");
+    insertOne: function (tableInput, columnInput, valueInput) {
+        // let columns = colAndValArray.keys().join(", ");
+        // let values = colAndValArray.values().join(", ");
         const queryString = "INSERT INTO ?? (??) values (?)";
         console.log(queryString);
-        connection.query(queryString, [tableInput, columns, values], function (err, result) {
+        connection.query(queryString, [tableInput, columnInput, valueInput], function (err, result) {
             if (err) throw err;
             console.log(result);
         });
     },
 
-    updateOne: function (tableInput, colAndValArray, whereSelector) {
-        let columns = colAndValArray.keys().join(", ");
-        let values = colAndValArray.values().join(", ");
-        let whereColumns = whereSelector.keys().join(", ");
-        let whereValues = whereSelector.values().join(", ");
+    updateOne: function (tableInput, columnInput, valueInput, whereColumn, whereValue) {
+        // let columns = colAndValArray.keys().join(", ");
+        // let values = colAndValArray.values().join(", ");
+        // let whereColumns = whereSelector.keys().join(", ");
+        // let whereValues = whereSelector.values().join(", ");
         const queryString = "UPDATE ?? SET ?? = ? WHERE ? = ?";
         console.log(queryString);
-        connection.query(queryString, [tableInput, columns, values, whereColumns, whereValues], function (err, result) {
+        connection.query(queryString, [tableInput, columns, values, whereColumn, whereValue], function (err, result) {
             if (err) throw err;
             console.log(result);
         });
