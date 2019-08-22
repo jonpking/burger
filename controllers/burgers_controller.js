fd048c7ca1ab9b92203508d167738fdb22fc28b5
Enigma <jonpking@gmail.com>
2019-08-21 18:47:04 -0700

    started router code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,57 @@
 const express = require("express");
 const burger = require("../models/burger.js");
+const router = express.Router();
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "name", "sleepy"
+    ], [
+            req.body.name, req.body.sleepy
+        ], function (result) {
+            // Send back the ID of the new quote
+            res.json({ id: result.insertId });
+        });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        sleepy: req.body.sleepy
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burger/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    burger.delete(condition, function (result) {
+        if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+
+module.exports = router;
