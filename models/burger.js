077e94077b3ddbed7104d5cd29f083fbe916a0b5
Enigma <jonpking@gmail.com>
2019-08-21 18:48:12 -0700

    made small syntax modification to each part of burger model

--- models/burger.js
+++ models/burger.js
@@ -2,22 +2,17 @@ const orm = require("../config/orm.js");
 
 const burger = {
     all: function (cb) {
-        orm.all("burgers", function (res) {
+        orm.selectAll("burgers", function (res) {
             cb(res)
         });
     },
     create: function (cols, vals, cb) {
-        orm.select("burgers", cols, vals, function (res) {
+        orm.insertOne("burgers", cols, vals, function (res) {
             cb(res);
         });
     },
     update: function (objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
-    },
-    delete: function (condition, cb) {
-        orm.delete("burgers", condition, function (res) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
             cb(res);
         });
     }
