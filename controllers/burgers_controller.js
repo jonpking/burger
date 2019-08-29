2e8223f579d5c8b239b9b37de2e516459cded98a
Enigma <jonpking@gmail.com>
2019-08-28 18:21:55 -0700

    updated routes functions

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,34 +3,21 @@ const burger = require("../models/burger.js");
 module.exports = function (app) {
 
     app.get("/", function (req, res) {
-        burger.all(function (data) {
-            const hbsObject = {
-                burgers: data
-            };
-            res.render("index", hbsObject);
+        burger.all(function (result) {
+            console.log("Results:", result);
+            res.render("index", {
+                burgers: result
+            });
         });
     });
 
     app.post("/api/burgers", function (req, res) {
-        burger.create([
-            "burger_name", "devoured"
-        ], [
-                req.body.burger_name, req.body.devoured
-            ], function (result) {
-                res.json({ id: result });
-            });
+        burger.create("burger_name", req.body.burger_name, function (result) {
+            res.json({ id: result });
+        });
     });
 
-    app.put("/api/burgers/:id", function (req, res) {
-        let condition = "id = " + req.params.id;
-        burger.update({
-            devoured: req.body.devoured
-        }, condition, function (result) {
-            if (result.changedRows == 0) {
-                return res.status(404).end();
-            } else {
-                res.status(200).end();
-            }
-        });
+    app.put("/api/burgers/:name", function (req, res) {
+        burger.update("devoured", true, "burger_name", req.params.name);
     });
 }
